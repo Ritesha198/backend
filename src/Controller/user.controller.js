2b8254105bff3996f633058fde7b76357dce562f
Ritesha198 <ritanc123@gmail.com>
2024-01-30 13:46:33 +0530

    register logics

--- src/Controller/user.controller.js
+++ src/Controller/user.controller.js
@@ -1,9 +1,68 @@
-import {asyncHandler} from "../Utils/asyncHandler.js"
+import { asyncHandler } from "../Utils/asyncHandler.js";
+import { ApiError } from "../Utils/ApiError.js";
+import { User } from "../Models/user.model.js";
+import { uploadOnCloudinary } from "../Utils/Cloudinary.js";
+import { ApiResponse } from "../Utils/ApiResponse.js";
 
-const registerUser = asyncHandler(async (req,res) => {
-    res.status(200).json({
-        message:"Ritesh is king"
-    })
-})
+const registerUser = asyncHandler(async (req, res) => {
+  //get user detail
+  //validation - not empty
+  //check if user already exist: email,username
+  //check for images and avtar
+  //upload to cloudinary
+  //create user object - create entry in db
+  //remove password and refresh token field from response
+  //check for user creation
+  //return res
 
-export {registerUser}
+  const { fullname, email, username, password } = req.body;
+  console.log("email", email);
+
+  if (
+    [fullname, email, username, password].same((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are requierd");
+  }
+
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (existedUser) {
+    throw new ApiError(409, "User with email or username already exist");
+  }
+  const avtarLocal = req.files?.avtar[0]?.path;
+  const coverImageLocal = req.files?.coverImage[0]?.path;
+  if (!avtarLocal) {
+    throw new ApiError(400, "Avatar is necessary");
+  }
+
+  const avtar = await uploadOnCloudinary(avtarLocal);
+  const coverImage = await uploadOnCloudinary(coverImageLocal);
+
+  if (!avtarLocal) {
+    throw new ApiError(400, "Avatar is necessary");
+  }
+
+  const user = await User.create({
+    fullname,
+    avtar: avtar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
+  const createUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+  if (!createUser) {
+    throw new ApiError(500, "something went wrong whiteregisterinh thr user");
+  }
+
+  return res.status(201).json(
+    new ApiResponse(200,createUser,"User regestered successfully")
+  )
+
+});
+
+export { registerUser };
--- src/Utils/Cloudinary.js
+++ src/Utils/Cloudinary.js
@@ -21,12 +21,8 @@ const uploadOnCloudinary = async (localFilePath) => {
     fs.unlinkSync(localFilePath); //remove locally saved temporary file as the upload got failed
     return null;
   }
-};
+}
 
-cloudinary.uploader.upload(
-  "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-  { public_id: "olympic_flag" },
-  function (error, result) {
-    console.log(result);
-  }
-);
+
+
+export {uploadOnCloudinary};
