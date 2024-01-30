45b988f69ff6988eed1a195b29039f62fc0cd326
Ritesha198 <ritanc123@gmail.com>
2024-01-30 14:52:49 +0530

    register done

--- src/Controller/user.controller.js
+++ src/Controller/user.controller.js
@@ -16,15 +16,15 @@ const registerUser = asyncHandler(async (req, res) => {
   //return res
 
   const { fullname, email, username, password } = req.body;
-  console.log("email", email);
+  console.log("body", req.body);
 
   if (
-    [fullname, email, username, password].same((field) => field?.trim() === "")
+    [fullname, email, username, password].some((field) => field?.trim() === "")
   ) {
     throw new ApiError(400, "All fields are requierd");
   }
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
 
@@ -32,10 +32,20 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(409, "User with email or username already exist");
   }
   const avtarLocal = req.files?.avtar[0]?.path;
-  const coverImageLocal = req.files?.coverImage[0]?.path;
+  //   const coverImageLocal = req.files?.coverImage[0]?.path;
+
+  let coverImageLocal;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocal = req.files.coverImage[0].path;
+  }
   if (!avtarLocal) {
     throw new ApiError(400, "Avatar is necessary");
   }
+  console.log("files", req.files);
 
   const avtar = await uploadOnCloudinary(avtarLocal);
   const coverImage = await uploadOnCloudinary(coverImageLocal);
@@ -59,10 +69,9 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(500, "something went wrong whiteregisterinh thr user");
   }
 
-  return res.status(201).json(
-    new ApiResponse(200,createUser,"User regestered successfully")
-  )
-
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createUser, "User regestered successfully"));
 });
 
 export { registerUser };
